74abdc80190f1e127c0ce876ee415c64f2b1f50b
David Torralba Goitia <david.torralba.goitia@gmail.com>
2017-06-24 12:44:33 +0100

    Open web browser automatically on start.
    toqueteos/webbrowser package implemented to open default browser
    on start and access http://localhost:12345 straight away.

--- main.go
+++ main.go
@@ -6,10 +6,12 @@ import (
 
 	"github.com/GeertJohan/go.rice"
 	"github.com/gorilla/mux"
+	"github.com/toqueteos/webbrowser"
 )
 
 func main() {
 	router := mux.NewRouter()
 	router.PathPrefix("/").Handler(http.FileServer(rice.MustFindBox("website").HTTPBox()))
+	webbrowser.Open("http://localhost:12345")
 	log.Fatal(http.ListenAndServe(":12345", router))
 }
