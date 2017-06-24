8d4e86c3fa2a76f0281cd2244e9f6c9778b9cee8
David Torralba Goitia <david.torralba.goitia@gmail.com>
2017-06-24 12:23:49 +0100

    Initial commit
    It contains the basic frontend and backend code. This example
    does't have any API end point.

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/GeertJohan/go.rice"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.PathPrefix("/").Handler(http.FileServer(rice.MustFindBox("website").HTTPBox()))
+	log.Fatal(http.ListenAndServe(":12345", router))
+}
---
+++ website/app.js
@@ -0,0 +1,1 @@
+document.getElementById("text").innerHTML = "Hello World<br /><p>Created by Nic Raboy</p>";
